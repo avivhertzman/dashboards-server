a75771d677066ff9909f1678b0c52179e4d754f4
AVIV <avivhertzman@gmail.com>
2023-07-23 18:46:01 +0300

    add default time field for event

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,12 +1,16 @@
 import { elasticAcessor } from "../elasticAccessor/elasticAccessor";
+import { format } from 'date-fns';
+
+const DATE_TIME_FORMAT = 'mm/dd/yy hh:mm:ss';
 
 export class EventService {
+
     constructor() {
     }
 
     createEvent(event: any) {
+        let date = format(new Date(), DATE_TIME_FORMAT);
+        event['timestamp'] = date;
         elasticAcessor.createEvent(event);
-
     }
-
 }
